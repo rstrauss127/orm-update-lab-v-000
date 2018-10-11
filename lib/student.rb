4d823f560078891dfa1ef0bb5c89abd23c48bc3a
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-11 21:12:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,4 +11,13 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT)
+        SQL
+    DB[:conn].execute(sql)
+  end  
 end
