5639e3aa282cfae36e766d74bc91de951888941e
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-11 21:31:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,4 +24,16 @@ class Student
   def self.drop_table
     DB[:conn].execute("DROP TABLE students")
   end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 end
