4dc35d3ecabc703fb8aa7847089311cb4b584d59
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-11 21:47:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,14 @@ class Student
   def self.new_from_db(row)
     student = self.new(id = row[0], name = row[1], grade = row[2])
     student
+  end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+      SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
