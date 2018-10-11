3a42fe55eb106558ac150dd49d32be754b845297
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-11 21:40:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,4 +42,12 @@ class Student
     student.save
     student
   end
+
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
 end
